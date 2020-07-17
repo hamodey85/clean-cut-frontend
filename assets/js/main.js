c5dbc4be00133c530805c87a2bd50c4129bfc48b
Mohammed Almajid <hamodey85@gmail.com>
2020-07-17 10:17:20 +1000

    uncomment js code

--- assets/js/main.js
+++ assets/js/main.js
@@ -160,58 +160,58 @@
       return false;
     });
 
-    // // Initiate the venobox plugin
-    // $(window).on('load', function() {
-    //   $('.venobox').venobox();
-    // });
+    // Initiate the venobox plugin
+    $(window).on('load', function() {
+      $('.venobox').venobox();
+    });
 
-    // // jQuery counterUp
-    // $('[data-toggle="counter-up"]').counterUp({
-    //   delay: 10,
-    //   time: 1000
-    // });
+    // jQuery counterUp
+    $('[data-toggle="counter-up"]').counterUp({
+      delay: 10,
+      time: 1000
+    });
 
-    // // Porfolio isotope and filter
-    // $(window).on('load', function() {
-    //   var portfolioIsotope = $('.portfolio-container').isotope({
-    //     itemSelector: '.portfolio-item',
-    //     layoutMode: 'fitRows'
-    //   });
+    // Porfolio isotope and filter
+    $(window).on('load', function() {
+      var portfolioIsotope = $('.portfolio-container').isotope({
+        itemSelector: '.portfolio-item',
+        layoutMode: 'fitRows'
+      });
 
-    //   $('#portfolio-flters li').on('click', function() {
-    //     $("#portfolio-flters li").removeClass('filter-active');
-    //     $(this).addClass('filter-active');
+      $('#portfolio-flters li').on('click', function() {
+        $("#portfolio-flters li").removeClass('filter-active');
+        $(this).addClass('filter-active');
 
-    //     portfolioIsotope.isotope({
-    //       filter: $(this).data('filter')
-    //     });
-    //     aos_init();
-    //   });
+        portfolioIsotope.isotope({
+          filter: $(this).data('filter')
+        });
+        aos_init();
+      });
 
-    //   // Initiate venobox (lightbox feature used in portofilo)
-    //   $(document).ready(function() {
-    //     $('.venobox').venobox();
-    //   });
-    // });
+      // Initiate venobox (lightbox feature used in portofilo)
+      $(document).ready(function() {
+        $('.venobox').venobox();
+      });
+    });
 
     // Portfolio details carousel
-    // $(".portfolio-details-carousel").owlCarousel({
-    //   autoplay: true,
-    //   dots: true,
-    //   loop: true,
-    //   items: 1
-    // });
+    $(".portfolio-details-carousel").owlCarousel({
+      autoplay: true,
+      dots: true,
+      loop: true,
+      items: 1
+    });
 
-    // // Init AOS
-    // function aos_init() {
-    //   AOS.init({
-    //     duration: 1000,
-    //     easing: "ease-in-out-back",
-    //     once: true
-    //   });
-    // }
-    // $(window).on('load', function() {
-    //   aos_init();
-    // });
+    // Init AOS
+    function aos_init() {
+      AOS.init({
+        duration: 1000,
+        easing: "ease-in-out-back",
+        once: true
+      });
+    }
+    $(window).on('load', function() {
+      aos_init();
+    });
 
   })(jQuery);
